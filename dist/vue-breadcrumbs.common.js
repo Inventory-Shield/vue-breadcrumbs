b20bc508adc5ab435f68fed490ff29723e685c59
Casper Lai <casperlai@Casperde-MacBook-Pro.local>
2016-10-05 15:34:12 +0800

    Upgrade vue 2.0
    Example also support vue 2.0

--- dist/vue-breadcrumbs.common.js
+++ dist/vue-breadcrumbs.common.js
@@ -1,19 +1,19 @@
 /*!
- * vue-breadcrumbs v0.3.0
+ * vue-breadcrumbs v0.3.1
  * (c) 2016 Sam Turrell
  * Released under the MIT License.
  */
 'use strict';
 
 function plugin(Vue) {
-  var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
   Object.defineProperties(Vue.prototype, {
     $breadcrumbs: {
       get: function get() {
         var crumbs = [];
         for (var i = 0; i < this.$route.matched.length; i++) {
-          if (this.$route.matched[i].handler && this.$route.matched[i].handler.breadcrumb) {
+          if (this.$route.matched[i].meta && this.$route.matched[i].meta.breadcrumb) {
             crumbs.push(this.$route.matched[i]);
           }
         }
@@ -23,10 +23,10 @@ function plugin(Vue) {
   });
 
   Vue.component('breadcrumbs', {
-    template: '<nav class="breadcrumbs" v-if="$breadcrumbs.length"> <ul> <li v-for="(i, crumb) in $breadcrumbs"> <a v-link="crumb">{{ crumb.handler.breadcrumb }}</a> </li> </ul> </nav>'
+    template: '<nav class="breadcrumbs" v-if="$breadcrumbs.length"> <ul> <li v-for="(crumb, i) in $breadcrumbs"> <router-link :to=" { path: crumb.path }">{{ crumb.meta.breadcrumb }}</router-link> </li> </ul> </nav>'
   });
 }
 
-plugin.version = '0.3.0';
+plugin.version = '0.3.1';
 
 module.exports = plugin;
