6eb4956edf61802efca202f557e61c5ff452b887
Casper Lai <casperlai@Casperde-MacBook-Pro.local>
2016-10-14 03:12:54 +0800

    2.0.0

--- dist/vue2-breadcrumbs.common.js
+++ dist/vue2-breadcrumbs.common.js
@@ -1,23 +1,32 @@
-function plugin (Vue, options = {}) {
+/*!
+ * vue2-breadcrumbs v2.0.0
+ * (c) 2016 Casper Lai
+ * Released under the MIT License.
+ */
+'use strict';
+
+function plugin(Vue) {
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
   Object.defineProperties(Vue.prototype, {
     $breadcrumbs: {
-      get: function () {
-        var crumbs = []
+      get: function get() {
+        var crumbs = [];
         for (var i = 0; i < this.$route.matched.length; i++) {
           if (this.$route.matched[i].meta && this.$route.matched[i].meta.breadcrumb) {
-            crumbs.push(this.$route.matched[i])
+            crumbs.push(this.$route.matched[i]);
           }
         }
-        return crumbs
+        return crumbs;
       }
     }
-  })
-  
+  });
+
   Vue.component('breadcrumbs', {
     template: '<nav class="breadcrumbs" v-if="$breadcrumbs.length"> <ul> <li v-for="(crumb, i) in $breadcrumbs"> <router-link :to=" { path: crumb.path }">{{ crumb.meta.breadcrumb }}</router-link> </li> </ul> </nav>'
-  })
+  });
 }
 
-plugin.version = '2.0.0'
+plugin.version = '2.0.0';
 
-export default plugin
+module.exports = plugin;
---
+++ dist/vue2-breadcrumbs.js
@@ -0,0 +1,38 @@
+/*!
+ * vue2-breadcrumbs v2.0.0
+ * (c) 2016 Casper Lai
+ * Released under the MIT License.
+ */
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  global.Vue2Breadcrumbs = factory();
+}(this, function () { 'use strict';
+
+  function plugin(Vue) {
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+    Object.defineProperties(Vue.prototype, {
+      $breadcrumbs: {
+        get: function get() {
+          var crumbs = [];
+          for (var i = 0; i < this.$route.matched.length; i++) {
+            if (this.$route.matched[i].meta && this.$route.matched[i].meta.breadcrumb) {
+              crumbs.push(this.$route.matched[i]);
+            }
+          }
+          return crumbs;
+        }
+      }
+    });
+
+    Vue.component('breadcrumbs', {
+      template: '<nav class="breadcrumbs" v-if="$breadcrumbs.length"> <ul> <li v-for="(crumb, i) in $breadcrumbs"> <router-link :to=" { path: crumb.path }">{{ crumb.meta.breadcrumb }}</router-link> </li> </ul> </nav>'
+    });
+  }
+
+  plugin.version = '2.0.0';
+
+  return plugin;
+
+}));
